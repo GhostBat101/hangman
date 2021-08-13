a22736b592c172aabf6c3138036d2551f3e0a76a
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-08-13 21:25:21 +0600

    Added more traits and implementation

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,50 @@ struct Word {
     representation: String
 }
 
+trait CheckLetter {
+    fn check_for_letter(&mut self, c: char) -> bool;
+}
+
+trait CheckComplete {
+    fn check_complete(&self) -> bool;
+}
+
+impl CheckComplete for Word {
+    fn check_complete(&self) -> bool {
+        self.correct_count == self.length
+    }
+}
+
+impl CheckLetter for Word {
+    fn check_for_letter(&mut self, c: char) -> bool {
+        let mut count: usize = 0;
+        let mut found: bool = false;
+        let mut response = String::with_capacity(self.length);
+        let mut index = 0;
+        for letter in self.answer.chars() {
+            if letter == c {
+                found = true;
+                count += 1;
+                response.push(c);
+            }
+            else {
+                if self.representation.chars().nth(index) != Some('_') {
+                    response.push(self.representation.chars().nth(index).unwrap());
+                }
+                else {
+                    response.push('_');
+                }
+            }
+            index += 1;
+        }
+        if found {
+            println!("Found a ")
+        }
+        self.representation = response;
+        self.correct_count += count;
+        count> 0
+    }
+}
 
 fn main() {
     println!("Hello, world!");
