646ed51962ee550fef7b1e705e58d3ff1f2b303e
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-08-13 16:00:11 +0600

    Added crates and struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate rand;
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use rand::Rng;
+
+struct Word {
+    answer: String,
+    length: usize,
+    correct_count: usize,
+    representation: String
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
